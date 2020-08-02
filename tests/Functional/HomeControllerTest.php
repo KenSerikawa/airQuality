a60cdfed9b2b1dc7b1d31dd7d282423ec2a415b1
KenSerikawa <kenserikawalucio@gmail.com>
2020-08-02 18:41:58 +0200

    Add functional testing for each method on HomeController

--- tests/Functional/HomeControllerTest.php
+++ tests/Functional/HomeControllerTest.php
@@ -5,32 +5,29 @@ declare(strict_types=1);
 namespace App\Tests\Functional;
 
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class HomeControllerTests extends KernelTestCase
+class HomeControllerTests extends WebTestCase
 {
-    /**
-     * @var \Doctrine\ORM\EntityManager
-     */
-    private $entityManager;
-
-    protected function setUp()
+    /** @test */
+    public function testIndex()
     {
-        $kernel = self::bootKernel();
-
-        $this->entityManager = $kernel->getContainer()
-            ->get('doctrine')
-            ->getManager();
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 
     /** @test */
-    public function testing()
+    public function testCitySearch()
     {
-        $this->assertEquals(1,1);
-    }
-
-    protected function tearDown(): void
-    {
-        $this->entityManager->close();
-        $this->entityManager = null;
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+        
+        $form = $crawler->selectButton('submit')->form();
+        $form['search'] = 'Madrid';
+        $crawler = $client->submit($form);
+        
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertSelectorTextContains('body > div.container.mb-5.pb-5.h-75 > div > div.d-flex.mb-2 > div.align-middle.my-auto.w-100 > h1', 'Madrid');
     }
 }
