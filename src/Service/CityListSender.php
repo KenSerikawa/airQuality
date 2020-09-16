f3cbdb9651922b68d762bad40c9a327fc5f4a549
KenSerikawa <kenserikawalucio@gmail.com>
2020-09-16 20:19:52 +0200

    Add city finder service

---
+++ src/Service/CityListSender.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Service;
+
+use Symfony\Component\HttpClient\HttpClient;
+
+class CityListSender {
+    public function __invoke()
+    {
+        $cities = [];
+        $listHandler = fopen(getcwd() . '/files/world_cities.csv', "r");
+        while (($data = fgetcsv($listHandler, 1000, ",")) !== FALSE) 
+        {		
+          $cities[] = $data[0];
+        }
+        fclose($listHandler);
+
+        return $cities;
+    }
+}
