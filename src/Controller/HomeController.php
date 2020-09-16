6f1a518b77ded995d4b52e3e2c29f1e6b9d69c76
KenSerikawa <kenserikawalucio@gmail.com>
2020-09-16 20:23:34 +0200

    Add cities request to construct method and set data on each view

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Service\AirQualityFinder;
+use App\Service\CityListSender;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,11 +11,16 @@ use Symfony\Component\HttpFoundation\Request;
 class HomeController extends AbstractController
 {
     public $client;
+    public $cities;
 
+    public function __construct(CityListSender  $cityListSender)
+    {
+        $this->cities = $cityListSender->__invoke();
+    }
     public function index(Request $request)
     {
         return $this->render('home/index.html.twig', [
-           
+            'cities' => $this->cities 
         ]);
     }
 
@@ -24,7 +30,8 @@ class HomeController extends AbstractController
         $json = $airQualityFinder($query);
         $data = json_decode($json);
         return $this->render('home/search.html.twig', [
-            'object' => $data
+            'object' => $data,
+            'cities' => $this->cities 
         ]);
     }
 }
