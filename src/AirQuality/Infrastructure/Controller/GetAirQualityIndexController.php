518917ca2a62a09b71d9461833782c084f72348a
KenSerikawa <kenserikawalucio@gmail.com>
2020-11-22 23:36:48 +0100

    add new unique action controller (SRP)

--- src/AirQuality/Infrastructure/Controller/GetAirQualityIndexController.php
+++ src/AirQuality/Infrastructure/Controller/GetAirQualityIndexController.php
@@ -4,7 +4,21 @@ declare(strict_types=1);
 
 namespace App\AirQuality\Infrastructure\Controller;
 
-final class GetAirQualityController
+use App\AirQuality\Application\CityListSender;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+final class GetAirQualityIndexController extends AbstractController
 {
     
+    public function __construct(CityListSender $cityListSender)
+    {
+        $this->cities = $cityListSender->__invoke();
+    }
+    
+    public function index()
+    {
+        return $this->render('home/index.html.twig', [
+            'cities' => $this->cities 
+        ]);
+    }
 }
