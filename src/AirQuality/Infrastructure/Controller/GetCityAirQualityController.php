20ed965ead35c0f2a15b9bd23cfc34f0729c5239
KenSerikawa <kenserikawalucio@gmail.com>
2020-11-23 16:45:51 +0100

    add cities and remove home dir from render path

--- src/AirQuality/Infrastructure/Controller/GetCityAirQualityController.php
+++ src/AirQuality/Infrastructure/Controller/GetCityAirQualityController.php
@@ -5,16 +5,19 @@ declare(strict_types=1);
 namespace App\AirQuality\Infrastructure\Controller;
 
 use App\AirQuality\Application\AirQualityFinder;
+use App\AirQuality\Application\CityListSender;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 
 final class GetCityAirQualityController extends AbstractController
 {
     private $finder; 
+    private $cities; 
 
-    public function __construct(AirQualityFinder $airQualityFinder)
+    public function __construct(AirQualityFinder $airQualityFinder, CityListSender $cityListSender)
     {
         $this->finder = $airQualityFinder;
+        $this->cities = $cityListSender->__invoke();
     }
 
     public function __invoke(Request $request)
@@ -22,9 +25,9 @@ final class GetCityAirQualityController extends AbstractController
         $query = $request->request->get('search');
         $json = $this->finder->__invoke($query);
         
-        return $this->render('home/search.html.twig', [
+        return $this->render('search.html.twig', [
             'object' => json_decode($json),
-            'cities' => [] 
+            'cities' => $this->cities
         ]);   
     }
 }
